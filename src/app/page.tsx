08e64150b3b93fdd9bb30d1a4eefc701210890b8
Alex Hortua <alexander@datzsolutions.com>
2024-10-21 19:27:07 -0600

    Adding NextUi to the applications as provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Alex</div>;
+// app/page.tsx
+import { Button } from '@nextui-org/button';
+
+export default function Page() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
